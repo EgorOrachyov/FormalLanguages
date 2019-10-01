46f8b25919081a434b1f007dc103572cd7c591b3
Alexandra Osipova <AlexOsipova13@gmail.com>
2019-10-01 10:32:47 +0300

    Implementation phase-2

--- src/main/java/LbaToGrammar.java
+++ src/main/java/LbaToGrammar.java
@@ -1,96 +1,215 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.*;
+import java.util.function.Consumer;
 
 public class LbaToGrammar {
+    private TuringMachine machine;
+    private Set<String> gamma;
+    private Set<String> statesL;
+    private Set<String> statesR;
+    private String L;
+    private String R;
     private List<Pair<String,String>> first = new ArrayList<>();
     private List<Pair<String,String>> main = new ArrayList<>();
     private List<Pair<String,String>> last = new ArrayList<>();
 
-    public Grammar convert(TuringMachine machine, Set<String> sigma, Set<String> posPairs) {
+    public LbaToGrammar(TuringMachine machine, Set<String> gamma, Set<String> statesL, Set<String> statesR) {
+        this.machine = machine;
+        this.gamma = gamma;
+        this.statesL = statesL;
+        this.statesR = statesR;
+        this.L = "%";
+        this.R = "$";
+    }
+
+    public LbaToGrammar(TuringMachine machine, Set<String> gamma, Set<String> statesL, Set<String> statesR, String L, String R) {
+        this.machine = machine;
+        this.gamma = gamma;
+        this.statesL = statesL;
+        this.statesR = statesR;
+        this.L = L;
+        this.R = R;
+    }
+
+    public Grammar convert() {
         String init = machine.getInit();
-        Set<String> gamma = machine.getGamma();
         Set<String> accept = machine.getAccept();
         Map<TuringMachine.Context, TuringMachine.Transition> transitions = machine.getTransitions();
 
-        String L = "%";
-        String R = "$";
-
-        addFirstRule("A1", "[,_]" + "[" + init + ",1,1]" + "A2");
-        addFirstRule("A2", "[1,1]A2");
-        addFirstRule("A2", "[,_]");
+        addFirstRule("A1", createVar(init, L, "1", "1") + "A2");
+        addFirstRule("A2", createVar("1", "1") + "A2");
+        addFirstRule("A2", createVar("1", "1", R));
 
         transitions.forEach((context, transition) -> {
             String s = context.getSymbol();
+            String q = context.getName();
+            String p = transition.getName();
+
             TuringMachine.Direction d = transition.getDirection();
+            Set<TuringMachine.Context> ctx = machine.getContext(p);
 
             if (d == TuringMachine.Direction.Right) {
-                if (s.equals("%")) {
-                    gamma.forEach(X -> {
-                         addRule(createVar("[" + context.getName() + "," + L, X, "1"),
-                                 createVar("[" + L + "," + transition.getName(), X, "1"));
+                if (s.equals(L)) {
+
+                    /////////////////////////// 1 ///////////////////////////
+
+                    forEachCtxGamma(ctx, c -> {
+                        addRule(createVar(q, L, c, "1"),
+                                createVar(L, p, c, "1"));
                     });
+
                 } else {
-                    gamma.forEach(Z -> {
-                        addRule(createVar(L + "," + context.getName(), context.getSymbol(), "1") + createVar(Z, "1"),
-                                createVar(L, transition.getSymbol(), "1") + createVar(transition.getName(), Z, "1"));
+
+                    forEachCtxGamma(ctx, c -> {
+
+                        /////////////////////////// 1 ///////////////////////////
+
+                        if (statesL.contains(q))
+                            addRule(createVar(L, q, s, "1") + createVar(c, "1"),
+                                    createVar(L, transition.getSymbol(), "1") + createVar(p, c, "1"));
+
+                        if (statesL.contains(q) && statesR.contains(p))
+                            addRule(createVar(L, q, s, "1") + createVar(c, "1", R),
+                                    createVar(L, transition.getSymbol(), "1") + createVar(p, c, "1", R));
+
+                        /////////////////////////// 2 ///////////////////////////
+
+                        addRule(createVar(q, s, "1") +
+                                        createVar(c, "1"),
+                                createVar(transition.getSymbol(), "1") +
+                                        createVar(p, c, "1"));
+
+
+                        if (statesR.contains(p))
+                            addRule(createVar(q, s, "1") +
+                                        createVar(c, "1", R),
+                                    createVar(transition.getSymbol(), "1") +
+                                        createVar(p, c, "1", R));
                     });
+
+                    /////////////////////////// 3 ///////////////////////////
+
+                    if (statesR.contains(q))
+                        addRule(createVar(q, s, "1", R),
+                                createVar(transition.getSymbol(), "1", p, R));
+
                 }
             } else if (d == TuringMachine.Direction.Stay) {
-                if (s.equals("%")) {
+                if (s.equals(L)) {
+
+                    /////////////////////////// 1 ///////////////////////////
+
                     gamma.forEach(X -> {
-                        addRule(createVar(context.getName(), L, X, "1"),
-                                createVar(transition.getName(), L, X, "1"));
+                        addRule(createVar(q, L, X, "1"),
+                                createVar(p, L, X, "1"));
                     });
+
+                } else if (s.equals(R)) {
+
+                    /////////////////////////// 3 ///////////////////////////
+
+                    gamma.forEach(X -> {
+                        addRule(createVar(X, "1", q, R),
+                                createVar(X, "1", p, R));
+                    });
+
                 } else {
-                    addRule(createVar(L, context.getName(), context.getSymbol(), "1"),
-                            createVar(L, transition.getSymbol(), "1"));
+
+                    /////////////////////////// 1 ///////////////////////////
+
+                    if (statesL.contains(q))
+                        addRule(createVar(L, q, s, "1"),
+                                createVar(L, transition.getSymbol(), "1"));
+
+                    /////////////////////////// 2 ///////////////////////////
+
+                    addRule(createVar(q, s, "1"),
+                            createVar(p, transition.getSymbol(), "1"));
+
+                    /////////////////////////// 3 ///////////////////////////
+
+                    if (statesR.contains(q))
+                        addRule(createVar(q, s, "1", R),
+                                createVar(transition.getSymbol(), "1", R));
+
                 }
             } else if (d == TuringMachine.Direction.Left) {
-                addRule(createVar(L, context.getName(), context.getSymbol(), "1"),
-                        createVar(transition.getName(), L, transition.getSymbol(), "1"));
+                if (s.equals(R)) {
+
+                    /////////////////////////// 3 ///////////////////////////
+
+                    forEachCtxGamma(ctx, c -> {
+                        addRule(createVar(c, "1", q, R),
+                                createVar(p, c, "1", R));
+                    });
+
+                } else {
+
+                    /////////////////////////// 1 ///////////////////////////
+
+                    if (statesL.contains(q))
+                        addRule(createVar(L, q, s, "1"),
+                                createVar(p, L, transition.getSymbol(), "1"));
+
+                    forEachCtxGamma(ctx, c -> {
+
+                        /////////////////////////// 2 ///////////////////////////
+
+                        addRule(createVar(c, "1") +
+                                        createVar(q, s, "1"),
+                                createVar(p, c, "1") +
+                                        createVar(transition.getSymbol(), "1"));
+
+                        if (statesL.contains(p))
+                            addRule(createVar(L, c, "1") +
+                                        createVar(q, s, "1"),
+                                    createVar(L,p, c, "1") +
+                                        createVar(transition.getSymbol(), "1"));
+
+                        /////////////////////////// 3 ///////////////////////////
+
+                        if (statesR.contains(q))
+                            addRule(createVar(c, "1") +
+                                        createVar(q, s, "1", R),
+                                    createVar(p, c, "1") +
+                                        createVar(transition.getSymbol(), "1", R));
+
+                    });
+                }
+
             }
         });
 
-        transitions.forEach((context, transition) -> {
-            String s = context.getSymbol();
-            TuringMachine.Direction d = transition.getDirection();
+        accept.forEach(q -> {
+            gamma.forEach(X -> {
 
-            if (d == TuringMachine.Direction.Right) {
-                gamma.forEach(Z -> {
-                    addRule(createVar(context.getName(), context.getSymbol(), "1") +
-                                    createVar(Z, "1"),
-                            createVar(transition.getSymbol(), "1") +
-                                    createVar(transition.getName(), Z, "1"));
+                addLastRule(createVar(q, L, X, "1"), "1");
+                addLastRule(createVar(L, q, X, "1"), "1");
 
-                    addRule(createVar(context.getName(), context.getSymbol(), "1") +
-                                    createVar(Z, "1", R),
-                            createVar(transition.getSymbol(), "1") +
-                                    createVar(transition.getName(), Z, "1", R));
-                });
-            } else if (d == TuringMachine.Direction.Stay) {
-                addRule(createVar(context.getName(), context.getSymbol(), "1"),
-                        createVar(transition.getName(), transition.getSymbol(), "1"));
-            } else if (d == TuringMachine.Direction.Left) {
-                gamma.forEach(Z -> {
-                    addRule(createVar(Z, "1") +
-                                    createVar(context.getName(), context.getSymbol(), "1"),
-                            createVar(transition.getName(), Z, "1") +
-                                    createVar(transition.getSymbol(), "1"));
+                addLastRule(createVar(q, X, "1"), "1");
 
-                    addRule(createVar(L, Z, "1") +
-                                    createVar(context.getName(), context.getSymbol(), "1"),
-                            createVar(L,transition.getName(), Z, "1") +
-                                    createVar(transition.getSymbol(), "1"));
-                });
-            }
-        });
+                addLastRule(createVar(q, X, "1", R), "1");
+                addLastRule(createVar(X, "1", q, R), "1");
 
-        transitions.forEach((context, transition) -> {
+                addLastRule("1" + createVar(X, "1"), "11");
+                addLastRule("1" + createVar(X, "1", R), "11");
 
+                addLastRule(createVar(X, "1") + "1", "11");
+                addLastRule(createVar(L, X, "1") + "1", "11");
+
+            });
         });
 
         return new Grammar("A1", first, main, last);
+    }
+
+    private void forEachCtxGamma(Set<TuringMachine.Context> ctx, Consumer<? super  String> action) {
+        ctx.forEach(c -> {
+            if (!c.getSymbol().equals(R) && !c.getSymbol().equals(L)) {
+                action.accept(c.getSymbol());
+            }
+        });
     }
 
     private void addRule(String left, String right) {
@@ -117,9 +236,42 @@ public class LbaToGrammar {
         return "[" + X + "," + a + "]";
     }
 
+    private boolean contains(Set<TuringMachine.Context> context, String symbol) {
+        for (TuringMachine.Context c : context) {
+            if (c.getSymbol().equals(symbol))
+                return true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) throws Exception {
         TuringMachine machine = new TuringMachineLoader().load(new FileInputStream("PrimeNumsLBA.txt"));
-        Grammar grammar = new LbaToGrammar().convert(machine, null, null);
+
+        Set<String> gamma = new HashSet<>();
+        gamma.add("1");
+        gamma.add("#");
+
+        Set<String> statesL = new HashSet<>();
+        statesL.add("init");
+        statesL.add("copy2");
+        statesL.add("finishCopy1");
+        statesL.add("moveLeft");
+        statesL.add("markSt");
+
+        Set<String> statesR = new HashSet<>();
+        statesR.add("markDiv");
+        statesR.add("finishCopy3");
+        statesR.add("checkInit");
+        statesR.add("copy5");
+        statesR.add("copy6");
+        statesR.add("incDiv3");
+        statesR.add("moveLeft");
+        statesR.add("copyDone1");
+        statesR.add("copyDone2");
+        statesR.add("copyDone3");
+
+        Grammar grammar = new LbaToGrammar(machine, gamma, statesL, statesR).convert();
         new GrammarSerialize().serialize(new FileOutputStream("PrimeNumsT1.txt"), grammar);
     }
 
